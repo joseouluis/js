9bb1cb5f2aedfadbaacdfa8f49c036ac8c460f2b
Joseph <jose.sa@metasix.com.br>
2018-03-07 15:45:56 -0300

    add getter usando 'get'

--- alura/ES6/aluraframe/client/js/app/models/Negociacao.js
+++ alura/ES6/aluraframe/client/js/app/models/Negociacao.js
@@ -8,21 +8,21 @@ class Negociacao {
 
     }
 
-    getVolume() {
+    get volume() {
 
         return this._quantidade * this._valor;
     
     }
     
-    getData() {
+    get data() {
         return this._data;
     }
     
-    getQuantidade() {
+    get quantidade() {
         return this._quantidade;
     }
     
-    getValor() {
+    get valor() {
         return this._valor;
     }
 }
