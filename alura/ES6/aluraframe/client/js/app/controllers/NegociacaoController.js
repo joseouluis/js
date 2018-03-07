2d0bd0caececbc7b8d8973b9277ad8d427065a86
Joseph <jose.sa@metasix.com.br>
2018-03-07 17:12:48 -0300

    criado constructor para percorrer o DOM apenas uma vez

--- alura/ES6/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ alura/ES6/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,7 +1,21 @@
 class NegociacaoController {
 
+    constructor() {
+  
+              let $ = document.querySelector.bind(document);
+  
+              this.inputData = $('#data');
+              this.inputQuantidade =  $('#quantidade');
+              this.inputValor = $('#valor');
+    }
+  
     adiciona(event) {
           event.preventDefault();
-          alert('Chamei ação no controller');
+  
+  
+          console.log(this.inputData.value);
+          console.log(this.inputQuantidade.value);
+          console.log(this.inputValor.value);
+  
     }
   }
