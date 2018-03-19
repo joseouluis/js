cecaf1332ab6b8a7866a3284997f55d074d8a418
Joseph <jose.sa@metasix.com.br>
2018-03-19 14:52:31 -0300

    adicionado a função reflect.aply

--- alura/ES6/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ alura/ES6/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -3,14 +3,17 @@ class NegociacaoController {
     constructor() {
   
         let $ = document.querySelector.bind(document);
+
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+
+        this._listaNegociacoes = new ListaNegociacoes(this, function(model) {
+            this._negociacoesView.update(model);
+        });
 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
-
         this._mensagem = new Mensagem();
         this._mensagemView = new MensagemView($('#mensagemView'));
         this._mensagemView.update(this._mensagem);
@@ -25,6 +28,17 @@ class NegociacaoController {
         this._mensagem.texto = 'Negociacao adicionada com sucesso';
         this._mensagemView.update(this._mensagem);  
     
+        this._limpaFormulario();
+    }
+
+    apaga() {
+
+        this._listaNegociacoes.esvazia();
+        this._negociacoesView.update(this._listaNegociacoes);
+    
+        this._mensagem.texto = 'Negociações apagadas com sucesso';
+        this._mensagemView.update(this._mensagem);
+
         this._limpaFormulario();
     }
     
--- alura/ES6/aluraframe/client/js/app/models/ListaNegociacoes.js
+++ alura/ES6/aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,15 +1,24 @@
 class ListaNegociacoes {
 
-    constructor() {
-        this._negociacoes = []
+    constructor( contexto, armadilha ) {
+        this._negociacoes = [];
+        this._armadilha = armadilha;
+        this._contexto = contexto;
     }
 
     adiciona( negociacao ) {
         this._negociacoes.push( negociacao );
+        Reflect.apply(this._armadilha, this._contexto, [this]);
     }
     
     get negociacoes() {
     
-        return this._negociacoes;
+        return [].concat( this._negociacoes );
+    }
+
+    esvazia() {
+
+        this._negociacoes = [];
+        Reflect.apply(this._armadilha, this._contexto, [this]);
     }
 }
