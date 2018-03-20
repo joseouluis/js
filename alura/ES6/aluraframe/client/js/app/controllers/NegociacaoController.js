ef1ad9874eb5f46c3a6b46ece189211e24e58ea7
Joseph <jose.sa@metasix.com.br>
2018-03-20 14:44:37 -0300

    adicionado o Proxy no controler

--- alura/ES6/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ alura/ES6/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,14 +1,33 @@
 class NegociacaoController {
 
     constructor() {
-  
+
         let $ = document.querySelector.bind(document);
 
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        // this._listaNegociacoes = new ListaNegociacoes( model => this._negociacoesView.update( model ));
+        let self = this;
+        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
+
+            get(target, prop, receiver) {
+
+                if (['adiciona', 'esvazia'].includes(prop) && typeof (target[prop]) == typeof (Function)) {
+
+                    return function () {
+
+                        console.log(`método '${prop}' interceptado`);
+
+                        Reflect.apply(target[prop], target, arguments);
+                        self._negociacoesView.update(target);
+
+                    }
+                }
+
+                return Reflect.get(target, prop, receiver);
+            }
+        });
 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
@@ -16,25 +35,25 @@ class NegociacaoController {
         this._mensagemView = new MensagemView($('#mensagemView'));
         this._mensagemView.update(this._mensagem);
     }
-  
+
     apaga() {
 
         this._listaNegociacoes.esvazia();
         this._negociacoesView.update(this._listaNegociacoes);
-    
+
         this._mensagem.texto = 'Negociações apagadas com sucesso';
-        this._mensagemView.update( this._mensagem );
+        this._mensagemView.update(this._mensagem);
     }
 
-    adiciona( event ) {
+    adiciona(event) {
 
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._negociacoesView.update(this._listaNegociacoes);
-    
+
         this._mensagem.texto = 'Negociacao adicionada com sucesso';
-        this._mensagemView.update(this._mensagem);  
-    
+        this._mensagemView.update(this._mensagem);
+
         this._limpaFormulario();
     }
 
@@ -42,17 +61,17 @@ class NegociacaoController {
 
         this._listaNegociacoes.esvazia();
         this._negociacoesView.update(this._listaNegociacoes);
-    
+
         this._mensagem.texto = 'Negociações apagadas com sucesso';
         this._mensagemView.update(this._mensagem);
 
         this._limpaFormulario();
     }
-    
+
     _criaNegociacao() {
 
         return new Negociacao(
-            DateHelper.textoParaData( this._inputData.value ),
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
