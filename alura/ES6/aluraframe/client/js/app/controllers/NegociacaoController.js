4f14b4e665ec553bfc7c67f8a9ef09249ec8592a
Joseph <jose.sa@metasix.com.br>
2018-03-22 16:18:46 -0300

    requisted the negociation week

--- alura/ES6/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ alura/ES6/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -39,17 +39,21 @@ class NegociacaoController {
 
         let xhr = new XMLHttpRequest();
 
-        xhr.open( 'GET', 'negociacoes/semana' )
+        xhr.open('GET', 'negociacoes/semana')
 
         xhr.onreadystatechange = () => {
-            if(xhr.readyState == 4) {
-                if(xhr.status == 200) {
-                    console.log('Obtendo as negociações do servidor.')
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    JSON.parse(xhr.responseText)
+                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
+                        .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                    this._mensagem.texto = 'Negociações importadas com sucesso.';
                 } else {
-                    console.log('Não foi possível obter as negociações do servidor.')
+                    console.log(xhr.responseText);
+                    this._mensagem.texto = 'Não foi possível obter as negociações.';
                 }
             }
-        }  
+        }
 
         xhr.send();
     }
