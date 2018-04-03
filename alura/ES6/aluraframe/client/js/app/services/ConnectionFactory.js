c9c647a22bdf6508988d757865efda0ca8861dfb
Joseph <joseluis.bsa@gmail.com>
2018-04-03 12:07:28 -0300

    changed the code to DAO pattern

--- alura/ES6/aluraframe/client/js/app/services/ConnectionFactory.js
+++ alura/ES6/aluraframe/client/js/app/services/ConnectionFactory.js
@@ -1,9 +1,12 @@
 
 var ConnectionFactory = (function () {
 
-    var stores = ['negociacoes'];
-    var version = 8;
-    var dbName = 'aluraframe';
+    const stores = ['negociacoes'];
+    const version = 4;
+    const dbName = 'aluraframe';
+
+    var connection = null;
+    var close = null;
 
     return class ConnectionFactory {
 
@@ -25,8 +28,15 @@ var ConnectionFactory = (function () {
 
                 openRequest.onsuccess = e => {
 
-                    resolve(e.target.result);
-                };
+                    if (!connection) {
+                        connection = e.target.result;
+                        close = connection.close.bind(connection);
+                        connection.close = function () {
+                            throw new Error('Você não pode fechar diretamente a conexão');
+                        };
+                    }
+                    resolve(connection);
+                }
 
                 openRequest.onerror = e => {
 
@@ -44,6 +54,14 @@ var ConnectionFactory = (function () {
                 if (connection.objectStoreNames.contains(store)) connection.deleteObjectStore(store);
                 connection.createObjectStore(store, { autoIncrement: true });
             });
+        }
+
+        static closeConnection() {
+
+            if (connection) {
+                Reflect.apply(close, connection, [])
+                connection = null;
+            }
         }
     }
 })();
