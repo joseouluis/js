535fac1a1573a6474085d9a17273b792a3f4d30e
Joseph <jose.sa@metasix.com.br>
2019-06-22 15:10:02 -0300

    removido varias coisas antigas

--- scripts_faculdade/07-aula/dom-05.js
+++
@@ -1,9 +0,0 @@
-debugger
-
-var botao = document.getElementById("botao");
-// obtem elemento que vai receber o evento
-var onButtonClick = function(){
-	botao.textContent = "Clicou !!";
-}
-// associa a funcao ao evento do elemento
-botao.addEventListener("click", onButtonClick);
